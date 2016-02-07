487adf2c1482e67332e4a18d1b62cc31efa65e49
venn177 <venn177@gmail.com>
2016-02-07 14:01:58 -0500

    Update helloworld.py

--- plugins/helloworld.py
+++ plugins/helloworld.py
@@ -3,11 +3,11 @@
 from . import bot
 
 @bot.listen('Hello')
-def helloworld(message):
+def helloworld(message, comment):
 	bot.reply(' world!')
 	print('Reply successfully sent! Hello world!')
 	
 @bot.catchall
-def testit(message):
+def testit(message, comment):
 	bot.reply(' world!')
 	print('ayy lmao')
